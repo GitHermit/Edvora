d03f4da00e7a7347365e71cc9abfc1cbbceb10b1
Anotherbadprogrammer <chase.sanchez@costleyentertainment.com>
2022-01-12 17:43:47 -0600

    Initial Commit

---
+++ Edvora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edvora
+//
+//  Created by Chase Sanchez on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Chase Sanchez on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
